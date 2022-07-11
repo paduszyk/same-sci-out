27fe47016c1a07a9624691216b4b37680904af55
Kamil Paduszyński <kpaduszy@gmail.com>
2022-07-11 14:20:06 +0200

    Add two simple admin-URLs-related methods to the base Model class

--- src/base/options/models.py
+++ src/base/options/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import *  # NOQA
+from django.urls import reverse
 
 
 class Model(models.Model):
@@ -7,3 +8,22 @@ class Model(models.Model):
 
     class Meta:
         abstract = True
+
+    @property
+    def admin_change_url(self):
+        """Reverse the object's admin change page URL."""
+        return reverse(
+            "admin:{}_{}_change".format(
+                self._meta.app_label,
+                self._meta.model_name,
+            ),
+            args=(self.id,),
+        )
+
+    def get_admin_change_link(self, content=None, **attrs):
+        """Return HTML code with a link to the object's admin change page."""
+        attrs.update({"href": self.admin_change_url})
+        return "<a {}>{}</a>".format(
+            " ".join(f'{attr}="{value}"' for attr, value in attrs.items()),
+            content or str(self),
+        )
