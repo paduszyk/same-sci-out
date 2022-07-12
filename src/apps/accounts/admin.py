2be314985ccd7fb5eb6948d91908c9ad01a272f3
Kamil Paduszyński <kpaduszy@gmail.com>
2022-07-12 09:46:48 +0200

    Add some extra utils to the admin of the User model
    The added utils are:
    - MissingDataFilter for retrieving the user objects with blank fields corresponding the personal data;
    - activate/deactivate_selected admin actions.

--- src/apps/accounts/admin.py
+++ src/apps/accounts/admin.py
@@ -1,9 +1,36 @@
+from django.contrib import messages
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
+from django.db.models import Q
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ngettext_lazy
 
 from base.options import admin
 
 from .models import User
+
+
+class MissingDataFilter(admin.SimpleListFilter):
+    """Filtering the User objects by their complete/incomplete data."""
+
+    parameter_name = "missing_data"
+    title = _("brakujące dane")
+
+    def lookups(self, request, model_admin):
+        """Return the filter lookups."""
+        return [(True, _("Tak")), (False, _("Nie"))]
+
+    def queryset(self, request, queryset):
+        """Update the changelist queryset based on the filter lookup selected."""
+        if (value := self.value()) in ["True", "False"]:
+            checked_fields, lookup = ["first_name", "last_name", "email"], Q()
+            for field in checked_fields:
+                lookup = lookup | Q(**{f"{field}__exact": ""})
+            if value == "True":
+                return queryset.filter(lookup)
+            else:
+                return queryset.exclude(lookup)
 
 
 @admin.register(User)
@@ -17,7 +44,79 @@ class UserAdmin(admin.ModelAdmin, UserAdmin):
         "email",
         "last_login",
     )
-    list_filter = ("is_active", "is_staff", "is_superuser")
+    list_filter = ("is_active", "is_staff", "is_superuser", MissingDataFilter)
+    actions = ("activate_selected", "deactivate_selected")
+
+    @admin.action(description=_("Aktywuj wybranych użytkowników"))
+    def activate_selected(self, request, queryset):
+        """Activate selected users."""
+        queryset.filter(is_active=False).update(is_active=True)
+
+        self.message_user(
+            request,
+            message=ngettext_lazy(
+                "Aktywowano wybranego użytkownika.",
+                "Aktywowano wybranych użytkowników.",
+                queryset.count(),
+            ),
+            level=messages.SUCCESS,
+        )
+
+    @admin.action(description=_("Dezaktywuj wybranych użytkowników"))
+    def deactivate_selected(self, request, queryset):
+        """Deactivate selected users."""
+        # Distinguish superusers from the regular users
+        superusers = queryset.filter(is_superuser=True)
+
+        if superusers.exists():
+            if queryset.count() == superusers.count():
+                # Only superusers selected
+                self.message_user(
+                    request,
+                    message=format_html(
+                        _("Nie można dezaktywować superużytkowników: %s.")
+                        % (
+                            ", ".join(
+                                su.get_admin_change_link(content=su.username)
+                                for su in superusers
+                            ),
+                        ),
+                    ),
+                    level=messages.ERROR,
+                )
+            else:
+                queryset.filter(is_superuser=False).update(is_active=False)
+
+                self.message_user(
+                    request,
+                    message=format_html(
+                        _(
+                            "Dezaktywowano %d z %d wybranych użytkowników. "
+                            "Nie można dezaktywować superużytkowników: %s."
+                        )
+                        % (
+                            queryset.count() - superusers.count(),
+                            queryset.count(),
+                            ", ".join(
+                                su.get_admin_change_link(content=su.username)
+                                for su in superusers
+                            ),
+                        ),
+                    ),
+                    level=messages.WARNING,
+                )
+        else:
+            queryset.update(is_active=False)
+
+            self.message_user(
+                request,
+                message=ngettext_lazy(
+                    "Dezaktywowano wybranego użytkownika.",
+                    "Dezaktywowano wybranych użytkowników.",
+                    queryset.count(),
+                ),
+                level=messages.SUCCESS,
+            )
 
 
 # Grouping users is not relevant for the project, therefore the built-in
