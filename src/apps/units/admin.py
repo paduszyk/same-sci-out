03b0eb960fd85d15119c22d7670769b9e4d19b72
Kamil Paduszyński <kpaduszy@gmail.com>
2022-07-14 19:42:02 +0200

    Add lists of related units to change lists of the units app models

--- src/apps/units/admin.py
+++ src/apps/units/admin.py
@@ -8,8 +8,19 @@ from .models import Department, Faculty, University
 class UniversityAdmin(admin.ModelAdmin):
     """Admin options and functionalities for the University model."""
 
-    list_display = ("name", "abbr")
+    list_display = ("name", "abbr", "faculty__list")
     search_fields = ("name", "abbr")
+
+    @admin.display(description=Faculty._meta.verbose_name_plural)
+    @as_html
+    def faculty__list(self, obj):
+        """Return a list of faculties related to the object."""
+        if (faculties := obj.faculties.all()).exists():
+            return "<br>".join(
+                faculty.get_admin_change_link(content="name") for faculty in faculties
+            )
+        else:
+            return "-"
 
 
 @admin.register(Faculty)
@@ -18,7 +29,7 @@ class FacultyAdmin(admin.ModelAdmin):
 
     autocomplete_fields = ("ancestor",)
 
-    list_display = ("name", "abbr", "university__name")
+    list_display = ("name", "abbr", "university__name", "department__list")
     search_fields = ("name", "abbr")
 
     @admin.display(
@@ -29,6 +40,17 @@ class FacultyAdmin(admin.ModelAdmin):
     def university__name(self, obj):
         """Return the faculty's university name."""
         return obj.university.get_admin_change_link(content="name")
+
+    @admin.display(description=Department._meta.verbose_name_plural)
+    @as_html
+    def department__list(self, obj):
+        """Return a list of departments related to the object."""
+        if (departments := obj.departments.all()).exists():
+            return "<br>".join(
+                department.get_admin_change_link(content="name")
+                for department in departments
+            )
+        return "-"
 
 
 @admin.register(Department)
