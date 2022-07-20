a79b5491d1aad1945898ded181a4629b4a93e392
Kamil Paduszyński <kpaduszy@gmail.com>
2022-07-20 09:29:42 +0200

    Implement to_dict method for the base Model objects

--- src/base/models.py
+++ src/base/models.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 from django.db import models
 from django.db.models import *  # NOQA
 
@@ -14,3 +16,45 @@ class Model(models.Model):
     def opts(cls):
         """Return meta options object for the model class."""
         return cls._meta
+
+    def to_dict(self, *, fields=None, exclude=None):
+        """Return a dict representation of the model instance."""
+        if fields and exclude:
+            raise ValueError(
+                "Specifying both 'fields' or 'exclude' is prohibited. "
+                "Use one of them while setting another as None."
+            )
+
+        opts = self.opts
+
+        # Get all the fields
+        all_fields = list(
+            chain(
+                opts.concrete_fields,
+                opts.private_fields,
+                opts.many_to_many,
+            )
+        )
+
+        # Validate the names of the fields passed as `fields` or `exclude` parameter
+        if fields or exclude:
+            for field in fields or exclude:
+                if field not in [field.name for field in all_fields]:
+                    raise KeyError(
+                        "Model {} has not a field '{}' given in '{}' parameter.".format(
+                            opts.label, field, "fields" if fields else "exclude"
+                        )
+                    )
+
+        # Apply filtering, collect & return the output dict
+        if fields:
+            all_fields = filter(lambda field: field.name in fields, all_fields)
+        if exclude:
+            all_fields = filter(lambda field: field.name not in exclude, all_fields)
+
+        return {
+            field.name: field.value_from_object(self)
+            if not field.many_to_one
+            else getattr(self, field.name)
+            for field in all_fields
+        }
